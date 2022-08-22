604a2e6df9abc93740edb7bb0901efe89507e819
Vitoldas Rapcevičius <vitoldas.rapcevicius@gmail.com>
2022-08-22 13:09:50 +0300

    Possibility do addition with %.

--- Udemy. WPF. Calculator/MainWindow.xaml.cs
+++ Udemy. WPF. Calculator/MainWindow.xaml.cs
@@ -54,10 +54,13 @@ namespace Udemy.WPF.Calculator
         
         private void PercentageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            double tempNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out tempNumber))
             {
-                lastNumber = lastNumber / 100;
-                resultLabel.Content = lastNumber.ToString();
+                tempNumber = tempNumber / 100;
+                if (lastNumber!=0)
+                    tempNumber *= lastNumber;
+                resultLabel.Content = tempNumber.ToString();
             }
         }
         //šis metodas prasisuka du kartus, todėl vietoje minuso atsiranda pliusas.
@@ -73,6 +76,8 @@ namespace Udemy.WPF.Calculator
         private void AcButton_Click(object sender, RoutedEventArgs e)
         {
             resultLabel.Content = "0";
+            result = 0;
+            lastNumber = 0;
         }
 
         private void OperationButton_Click(object sender, RoutedEventArgs e)
